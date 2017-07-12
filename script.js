7da5fea9a5cd8f347e19ebe0111396b3f6edda3b
Zack Hartmann <zackhartmann@gmail.com>
2017-07-12 17:18:09 -0400

    bonus.txt edited

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 var todoList = {
-  todos: ['item 1', 'item 2', 'item 3'],
+  // instantiate blank array, rather than array of strings.
+  todos: [],
   displayTodos: function() {
     console.log('My Todos', this.todos);
   },
-  addTodo: function(todo) {
-    this.todos.push(todo);
+  // Create and 'push()' Object in lieu of a String.
+  // passed in "todoText" becomes a string property of the object.
+  addTodo: function(todoText) {
+    this.todos.push({
+      // said object
+      todoText: todoText,
+      // new boolean property
+      // object incomplete by default
+      completed: false
+    });
     this.displayTodos();
   },
+  // changeTodos method
   changeTodo: function(position, newValue) {
     this.todos[position] = newValue;
     this.displayTodos();
   },
-  deleteTodo: function(posistion) {
+  // deleteTodos method
+  deleteTodo: function(position) {
     this.todos.splice(position, 1);
     this.displayTodos();
   }

e70e0b088bf64b1937b644dd1282ce8ae20a0ba1
Zack Hartmann <zackhartmann@gmail.com>
2017-07-18 14:40:59 -0400

    V10 Completed

--- script.js
+++ script.js
@@ -6,24 +6,21 @@ var todoList = {
       completed: false
     });
   },
-
   changeTodo: function(position, todoText) {
     this.todos[position].todoText = todoText;
   },
-
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
   },
-
   toggleCompleted: function(position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
   },
-
   toggleAll: function() {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
-    for (var i = 0; i < totalTodos; i++ ) {
+
+		for (var i = 0; i < totalTodos; i++ ) {
       if (this.todos[i].completed === true) {
         completedTodos++;
       }
@@ -40,7 +37,6 @@ var todoList = {
   }
 };
 
-// New handlers object contains methods which index.html refers to directly.
 var handlers = {
   displayTodos: function() {
     todoList.displayTodos();
@@ -63,10 +59,12 @@ var handlers = {
     changeTodoTextInput.value = '';
     view.displayTodos();
   },
-  deleteTodo: function() {
-    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-    deleteTodoPositionInput.value = '';
+  deleteTodo: function(position) {
+    // var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+		// replaces getElementById, because ID no longer comes from input text box.
+    todoList.deleteTodo(position);
+    // deleteTodoPositionInput.value = '';
+
     view.displayTodos();
   },
   toggleCompleted: function() {
@@ -76,15 +74,13 @@ var handlers = {
   }
 };
 
-// new view object for displaying todos
 var view = {
   displayTodos: function() {
     var todosUl = document.querySelector('ul');
-    todosUl.innerHTML = ''; // clears out unordered list before adding new li
+    todosUl.innerHTML = '';
     for (var i = 0; i < todoList.todos.length; i++) {
       var todoLi = document.createElement('li');
       var todo = todoList.todos[i];
-      // new variable for creating checkboxes display
       var todoTextWithCompletion = '';
 
       if (todo.completed === true) {
@@ -93,9 +89,37 @@ var view = {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
 
-      // stores the checkboxes as property of todoLi
+			// Assigns each todoLi a unique ID based on the for loop index.
+			todoLi.id = i;
       todoLi.textContent = todoTextWithCompletion;
+			// Appends delete buttons to page using createDeleteButton method.
+			todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
     }
-  }
+	},
+		// New Delete button for each line item.
+		createDeleteButton: function() {
+			var deleteButton = document.createElement('button');
+			deleteButton.textContent = 'Delete';
+			deleteButton.className = 'deleteButton';
+			return deleteButton;
+  },
+	// New method sets up event listeners.
+	setUpEventListeners: function() {
+		// New todosUl selects "ul" elements in the document for scrutiny.
+		var todosUl = document.querySelector('ul');
+		// New event listener runs delete function when the li button is pressed.
+		todosUl.addEventListener('click', function(event) {
+			// event.target.parentNode.id returns clicked ul element's associated ID.
+			var elementClicked = event.target;
+			// Delegation - single event listener responds to all clicks, but uses if
+			// statement to verify if the click event occurred on a delete button.
+			if (elementClicked.className === 'deleteButton') {
+				handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+			}
+		});
+	}
 };
+
+// view handler setUpEventListeners
+view.setUpEventListeners();
